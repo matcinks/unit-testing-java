8420cf61449a985974b719c52e68e36a2e20f1e6
matcinks <x.mateuszstepien@gmail.com>
2023-10-02 18:41:16 +0200

    Testing code performance with assertTimeout()

---
+++ src/test/java/pl/mat/testing/CartTest.java
@@ -0,0 +1,22 @@
+package pl.mat.testing;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartTest {
+
+    @Test
+    void simulateLargeOrder() {
+
+        //given
+        Cart cart = new Cart();
+
+        //when
+        //then
+        assertTimeout(Duration.ofMillis(100), cart::simulateLargeOrder);
+
+    }
+}

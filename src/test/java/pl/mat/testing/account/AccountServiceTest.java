ac5272d83c9435eae05f54b397cc4059d1a65e0b
matcinks <x.mateuszstepien@gmail.com>
2023-10-22 22:17:30 +0200

    Stubs replaced with mocks.

--- src/test/java/pl/mat/testing/account/AccountServiceTest.java
+++ src/test/java/pl/mat/testing/account/AccountServiceTest.java
@@ -2,20 +2,26 @@ package pl.mat.testing.account;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
-class AccountServiceTest {
+public class AccountServiceTest {
 
     @Test
     void getAllActiveAccounts() {
 
         //given
-        AccountRepository accountRepositoryStub = new AccountRepositoryStub();
-        AccountService accountService = new AccountService(accountRepositoryStub);
+        List<Account> accounts = prepareAccountData();
+        AccountRepository accountRepository = mock(AccountRepository.class);
+        AccountService accountService = new AccountService(accountRepository);
+//        when(accountRepository.getAllAccounts()).thenReturn(accounts);            // option 1
+        given(accountRepository.getAllAccounts()).willReturn(accounts);             // option 2
 
         //when
         List<Account> accountList = accountService.getAllActiveAccounts();
@@ -24,4 +30,36 @@ class AccountServiceTest {
         assertThat(accountList, hasSize(2));
 
     }
+
+    @Test
+    void getNoActiveAccounts() {
+
+        //given
+        AccountRepository accountRepository = mock(AccountRepository.class);
+        AccountService accountService = new AccountService(accountRepository);
+//        when(accountRepository.getAllAccounts()).thenReturn(Collections.emptyList());           // option 1
+        given(accountRepository.getAllAccounts()).willReturn(Collections.emptyList());            // option 2
+
+        //when
+        List<Account> accountList = accountService.getAllActiveAccounts();
+
+        //then
+        assertThat(accountList, hasSize(0));
+
+    }
+
+    private List<Account> prepareAccountData() {
+        Address address1 = new Address("Kwiatowa", "33/5" );
+        Account account1 = new Account(address1);
+
+        Account account2 = new Account();
+
+        Address address2 = new Address("Piekarska", "12b");
+        Account account3 = new Account(address2);
+
+        return Arrays.asList(account1, account2, account3);
+    }
+
+
+
 }
